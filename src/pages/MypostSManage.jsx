5a39ad2faa87b1476502b1576f6b77f61ac91b85
mubashirbappi <mubashirbappi@gmail.com>
2024-12-24 16:09:44 +0600

    data load in my manage post

--- src/pages/MypostSManage.jsx
+++ src/pages/MypostSManage.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { Authcontext } from '../Context/AuthContext/AuthProvider';
 
 const MypostSManage = () => {
+    const {user} = useContext(Authcontext)
+       useEffect(()=>{
+        fetch(`http://localhost:8000/posts?email=${user?.email}`)
+       .then(res=>res.json())
+       .then(data=>{
+        console.log(data)
+       })
+
+       },[])
+      
     return (
         <div>
             <div>
