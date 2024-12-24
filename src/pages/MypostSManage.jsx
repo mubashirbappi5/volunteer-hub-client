fc0618d8b99c75685a306ba30d21189b181ae8da
mubashirbappi <mubashirbappi@gmail.com>
2024-12-24 21:54:42 +0600

    update modal added using flwvite

--- src/pages/MypostSManage.jsx
+++ src/pages/MypostSManage.jsx
@@ -3,11 +3,12 @@ import { Authcontext } from "../Context/AuthContext/AuthProvider";
 import { Link } from "react-router-dom";
 import nodata from "../assets/Animation - 1735041941677.json";
 import Lottie from "lottie-react";
-import { Tabs } from "flowbite-react";
-import { Tab, TabList, TabPanel } from "react-tabs";
-import "react-tabs/style/react-tabs.css";
+import DatePicker from "react-datepicker";
+
+import { Modal, Button } from "flowbite-react";
 
 const MypostSManage = () => {
+  const [startDate, setStartDate] = useState(new Date());
   const { user } = useContext(Authcontext);
   const [myposts, setmypost] = useState([]);
   useEffect(() => {
@@ -31,14 +32,19 @@ const MypostSManage = () => {
       method: "DELETE",
       headers: { "Content-Type": "application/json" },
     })
-   
-   
-    .then(res=>res.json())
-    .then(data=>{
-        const remaingsmypost =myposts.filter(mypost => mypost._id !== id)
-        setmypost(remaingsmypost)
-        console.log('delete done')})
-  }
+      .then((res) => res.json())
+      .then((data) => {
+        const remaingsmypost = myposts.filter((mypost) => mypost._id !== id);
+        setmypost(remaingsmypost);
+        console.log("delete done");
+      });
+  };
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleModalToggle = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <div>
@@ -216,7 +222,10 @@ const MypostSManage = () => {
                               </p>
                             </td>
                             <td className="p-3 text-right">
-                              <Link className="btn btn-sm">Update</Link>
+                             
+                              <button  className="btn btn-sm" onClick={handleModalToggle}>
+                              Update
+                              </button>
                             </td>
                             <td className="p-3 ">
                               <button
@@ -236,6 +245,145 @@ const MypostSManage = () => {
             </div>
           </div>
         </div>
+      </div>
+
+      {/* -------------------------- */}
+      <div>
+        <Modal
+          className="bg-black/40  "
+          show={isOpen}
+          onClose={handleModalToggle}
+        >
+          <Modal.Header className="bg-white dark:bg-gray-700 border-b">
+            Update Your Post
+          </Modal.Header>
+          <Modal.Body className="bg-white dark:bg-gray-700 shadow rounded-lg ">
+            <div className=" px-4 mx-auto max-w-2xl ">
+              <form>
+                <div className="grid gap-4 lg:grid-cols-3 sm:grid-cols-2 sm:gap-6">
+                  <div className="sm:col-span-3">
+                    <label className="block mb-1 text-sm font-medium text-gray-900 dark:text-white">
+                      Post Title
+                    </label>
+                    <input
+                      type="text"
+                      name="title"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                      placeholder="Title"
+                      required=""
+                    />
+                  </div>
+                  <div className="w-full">
+                    <label className="block mb-1 text-sm font-medium text-gray-900 dark:text-white">
+                      Thumbnail{" "}
+                    </label>
+                    <input
+                      type="text"
+                      name="thumbnail "
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                      placeholder="Enter Thumbnail url"
+                      required=""
+                    />
+                  </div>
+                  <div className="w-full">
+                    <label className="block mb-1 text-sm font-medium text-gray-900 dark:text-white">
+                      Location
+                    </label>
+                    <input
+                      type="text"
+                      name="location"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                      placeholder="Enter Location"
+                      required=""
+                    />
+                  </div>
+                  <div className="w-full">
+                    <label className="block mb-1 text-sm font-medium text-gray-900 dark:text-white">
+                      Organizer name{" "}
+                    </label>
+                    <input
+                      type="text"
+                      name="organizer_name "
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                      value={user?.displayName}
+                      required=""
+                    />
+                  </div>
+                  <div className="w-full">
+                    <label className="block mb-1 text-sm font-medium text-gray-900 dark:text-white">
+                      organizer email{" "}
+                    </label>
+                    <input
+                      type="text"
+                      name="organizer_email"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                      value={user?.email}
+                      required=""
+                    />
+                  </div>
+                  <div class="w-full">
+                    <label className="block mb-1 text-sm font-medium text-gray-900 dark:text-white">
+                      {" "}
+                      volunteers needed
+                    </label>
+                    <input
+                      type="number"
+                      name="volunteersNeeded"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                      placeholder="No. of volunteers needed 
+       "
+                      required=""
+                    />
+                  </div>
+                  <div>
+                    <label className="block mb-1 text-sm font-medium text-gray-900 dark:text-white">
+                      Category
+                    </label>
+                    <select
+                      name="category"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                    >
+                      <option selected="">Select category</option>
+                      <option value="Healthcare">Healthcare</option>
+                      <option value="Education">Education</option>
+                      <option value="Service">Social Service</option>
+                      <option value="Animal welfare">Animal Welfare</option>
+                    </select>
+                  </div>
+                  <div className="sm:col-span-1">
+                    <label className="block mb-1 text-sm font-medium text-gray-900 dark:text-white">
+                      {" "}
+                      Deadline
+                    </label>
+                    <DatePicker
+                      className="w-40 rounded-lg border-opacity-5"
+                      selected={startDate}
+                      onChange={(date) => setStartDate(date)}
+                    />
+                  </div>
+                  <div class="sm:col-span-2">
+                    <label className="block mb-1 text-sm font-medium text-gray-900 dark:text-white">
+                      Description
+                    </label>
+                    <textarea
+                      name="description"
+                      rows="2"
+                      className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                      placeholder="Your description here"
+                    ></textarea>
+                  </div>
+                </div>
+                <div className="flex justify-center items-center my-6">
+                  <input
+                    className="btn btn-wide"
+                    type="submit"
+                    value="Update Post"
+                  />
+                </div>
+              </form>
+            </div>
+          </Modal.Body>
+        </Modal>
       </div>
     </div>
   );
