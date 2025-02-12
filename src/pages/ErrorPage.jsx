033b8dda4c741aee1f4e9fa661c997fccef45d89
mubashirbappi <mubashirbappi@gmail.com>
2025-02-12 15:41:40 +0600

    error btn modify

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import notfound from '../assets/Animation - 1735134283125.json'
 import Lottie from 'lottie-react';
+import { Link } from 'react-router-dom';
 const ErrorPage = () => {
     return (
         <div>
 
             <Lottie className='md:w-96 mx-auto' animationData={notfound}></Lottie>
-             <Link to={'/'}>Back to Home</Link>
+           <div className='flex justify-center'>
+           <Link className='btn bg-orange-300' to={'/'}>Back to Home</Link>
+           </div>
         </div>
     );
 };
